b4ccb31cf44be17ed89a92b28e6b309d4001f4ff
DD <daniel.13.davis@gmail.com>
2019-01-09 08:30:22 -0600

    Scroll Activation Adjusted

--- js/main.js
+++ js/main.js
@@ -51,13 +51,21 @@ let unfocusNav = function() {
   let documentElem = $(document);
   let aboutMeTitle = $('.about-me h3'), aboutMeTitleOffset = aboutMeTitle.offset().top/2;
   let aboutMeImage = $('.about-me .img-choose'), aboutMeImageOffset = aboutMeImage.offset().top/2;
-  let aboutMeP = $('.about-me .about-me-p'), aboutMePOffset = aboutMeP.offset().top/2;
+  let aboutMeYellowSp = $('.about-me .about-me-p .yellow-spacer'), aboutMeYellowSpOffset = aboutMeYellowSp.offset().top/2;
+  let aboutMeBase = $('.about-me .about-me-p .base'), aboutMeBaseOffset = aboutMeBase.offset().top/2;
+  let aboutMeYellowSpP = $('.about-me .about-me-p .yellow-spacer-p'), aboutMeYellowSpPOffset = aboutMeYellowSpP.offset().top/2;
+  let aboutMeQuote = $('.about-me .about-me-p .quote'), aboutMeQuoteOffset = aboutMeQuote.offset().top/2;
   documentElem.on('scroll', function() {
     if (documentElem.scrollTop() > aboutMeTitleOffset && aboutMeTitle.hasClass('hidden')) aboutMeTitle.removeClass('hidden');
 
     if (documentElem.scrollTop() > aboutMeImageOffset && aboutMeImage.hasClass('hidden')) aboutMeImage.removeClass('hidden');
 
-    if (documentElem.scrollTop() > aboutMePOffset && aboutMeP.hasClass('hidden')) aboutMeP.removeClass('hidden');
+    if (documentElem.scrollTop() > aboutMeYellowSpOffset && aboutMeYellowSp.hasClass('hidden')) aboutMeYellowSp.removeClass('hidden');
 
+    if (documentElem.scrollTop() > aboutMeBaseOffset && aboutMeBase.hasClass('hidden')) aboutMeBase.removeClass('hidden');
+
+    if (documentElem.scrollTop() > aboutMeYellowSpPOffset && aboutMeYellowSpP.hasClass('hidden')) aboutMeYellowSpP.removeClass('hidden');
+
+    if (documentElem.scrollTop() > aboutMeQuoteOffset && aboutMeQuote.hasClass('hidden')) aboutMeQuote.removeClass('hidden');
   });
 })();
